28a27438962ea1357ba84d5302ef189a2b874e98
Juanbernaortega <36245670+Juanbernaortega@users.noreply.github.com>
2018-04-24 07:16:07 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const vm = new Vue({
+    el: 'main',
+    data:  {
+        lim_preguntas: 2,
+        preguntas: {
+        	pregunta: 'pregunta 1',
+        	descripcion: 'estas en una pregunta',
+        }
+
+        preguntas: {
+        	pregunta: 'pregunta 1',
+        	descripcion: 'estas en una pregunta',
+        }
+    }
+    },
+});
